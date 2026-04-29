37cfcbf2924bdeb329d7eae97f6ef60a13b12057
Hubert Wiktorowicz <hubertwiktorowicz1@gmail.com>
2026-04-29 21:43:38 +0200

    Trivia DB

--- kurs/classes/quiz_project/data.py
+++ kurs/classes/quiz_project/data.py
@@ -1,14 +1,41 @@
 question_data = [
-{"text": "A slug's blood is green.", "answer": "True"},
-{"text": "The loudest animal is the African Elephant.", "answer": "False"},
-{"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-{"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-{"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
-{"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
-{"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-{"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-{"text": "Google was originally called 'Backrub'.", "answer": "True"},
-{"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-{"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-{"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
-]
+
+    {"type": "boolean", "difficulty": "medium", "category": "Sports",
+     "question": "During Wimbledon, spectators in the grounds can buy the tennis balls that have been used in matches.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Sports",
+     "question": "Skateboarding was included in the 2020 Summer Olympics in Tokyo.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Sports",
+     "question": "ATP tennis hosted several tournaments on carpet court before being replaced to reduce injuries.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Sports",
+     "question": "The first episode of WWF Monday Night RAW aired on January 11, 1990.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Sports",
+     "question": "Wilt Chamberlain scored his infamous 100-point-game against the New York Knicks in 1962.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Sports",
+     "question": "Formula E is an auto racing series that uses hybrid electric race cars.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Sports",
+     "question": "In 2008, Usain Bolt set the world record for the 100 meters with one shoelace untied.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Sports",
+     "question": "The Olympics tennis court is a giant green screen.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Sports",
+     "question": "Soccer player Cristiano Ronaldo opened a museum dedicated to himself.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Sports",
+     "question": "Tennis was once known as Racquetball.", "correct_answer": "False",
+     "incorrect_answers": ["True"]}]
