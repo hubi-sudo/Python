5c2c0730fd0fa83a76c43a8261c0becc1aa6b531
Hubert Wiktorowicz <hubertwiktorowicz1@gmail.com>
2026-05-08 15:18:14 +0200

    turtle_more

--- kurs/turtle_project/main.py
+++ kurs/turtle_project/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+from random import choice
 
 timmy = Turtle()
 timmy.shape("turtle")
@@ -12,6 +13,46 @@ def square():
         timmy.right(90)
     screen.exitonclick()
 
+
+def dashed_line():
+    for i in range(50):
+        timmy.forward(10)
+        timmy.penup()
+        timmy.forward(10)
+        timmy.pendown()
+
+
+colors = ["red", "green", "blue", "violet", "brown", "black", "purple", "orange"]
+
+def shapes():
+    angles = 3
+    while angles < 10:
+        timmy.pencolor(choice(colors))
+        for i in range(3, 3+ angles):
+            timmy.forward(100)
+            timmy.right(360/angles)
+        angles += 1
+    screen.exitonclick()
+
+angles = [0, 90, 180, 270]
+
+def random_walk():
+    timmy.width(20)
+    timmy.speed(6)
+    for i in range (100):
+        timmy.pencolor(choice(colors))
+        timmy.forward(50)
+        timmy.right(choice(angles))
+    screen.exitonclick()
+
+
+
 screen = Screen()
 
-square()
+#square()
+
+#dashed_line()
+
+#shapes()
+
+random_walk()
